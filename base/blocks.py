ce916de6146f4f7c3e38e63c844d8588829467ac
pearsedarcy <pearse.darcy@outlook.com>
2025-01-20 19:57:32 +0100

    Fix: Update db and add card2

--- base/blocks.py
+++ base/blocks.py
@@ -4,10 +4,10 @@ from wagtail.blocks import (
     RichTextBlock,
     StreamBlock,
     StructBlock,
+    URLBlock,
 )
 from wagtail.embeds.blocks import EmbedBlock
 from wagtail.images.blocks import ImageBlock
-
 
 class CaptionedImageBlock(StructBlock):
     image = ImageBlock(required=True)
@@ -18,6 +18,24 @@ class CaptionedImageBlock(StructBlock):
         icon = "image"
         template = "base/blocks/captioned_image_block.html"
 
+
+
+class SubHeadingBlock(StructBlock):
+    sub_heading_text = CharBlock(classname="sub-title", required=True)
+    size = ChoiceBlock(
+        choices=[
+            ("", "Select a heading size"),
+            ("h2", "H2"),
+            ("h3", "H3"),
+            ("h4", "H4"),
+        ],
+        blank=True,
+        required=False,
+    )
+
+    class Meta:
+        icon = "title"
+        template = "base/blocks/sub_heading_block.html"
 
 class HeadingBlock(StructBlock):
     heading_text = CharBlock(classname="title", required=True)
@@ -36,9 +54,37 @@ class HeadingBlock(StructBlock):
         icon = "title"
         template = "base/blocks/heading_block.html"
 
+class ButtonBlock(StructBlock):
+    button_text = CharBlock(required=False)
+    button_url = URLBlock(required=False)
+    button_style = ChoiceBlock(
+        choices=[
+            ('primary', 'Primary'),
+            ('secondary', 'Secondary'),
+        ],
+        default='primary',
+        blank=True,
+        required=False,
+    )
+
+    class Meta:
+        icon = "link"
+        template = "base/blocks/button_block.html"
 
 class BaseStreamBlock(StreamBlock):
     heading_block = HeadingBlock()
+    paragraph_block = RichTextBlock(icon="pilcrow")
+    image_block = CaptionedImageBlock()
+    button_block = ButtonBlock(icon="link")
+    embed_block = EmbedBlock(
+        help_text="Insert a URL to embed. For example, https://www.youtube.com/watch?v=SGJFWirQ3ks",
+        icon="media",
+    button_block = ButtonBlock(icon="link")
+    )
+
+class newStreamBlock(StreamBlock):
+    heading_block = HeadingBlock()
+    sub_heading_block = HeadingBlock()
     paragraph_block = RichTextBlock(icon="pilcrow")
     image_block = CaptionedImageBlock()
     embed_block = EmbedBlock(
--- portfolio/blocks.py
+++ portfolio/blocks.py
@@ -5,6 +5,8 @@ from wagtail.blocks import (
     PageChooserBlock,
     RichTextBlock,
     StructBlock,
+    URLBlock,
+    ChoiceBlock,
 )
 
 # import ImageBlock:
@@ -12,15 +14,29 @@ from wagtail.images.blocks import ImageBlock
 
 from base.blocks import BaseStreamBlock
 
+
+
 # add CardBlock:
 class CardBlock(StructBlock):
     heading = CharBlock()
+    sub_heading = CharBlock(required=False)
     text = RichTextBlock(features=["bold", "italic", "link"])
     image = ImageBlock(required=False)
 
     class Meta:
         icon = "form"
         template = "portfolio/blocks/card_block.html"
+
+class CardBlock2(StructBlock):
+    card_tag = CharBlock(required=False)
+    heading = CharBlock()
+    sub_heading = CharBlock(required=False)
+    text = RichTextBlock(features=["bold", "italic", "link"])
+    image = ImageBlock()
+
+    class Meta:
+        icon = "form"
+        template = "portfolio/blocks/card_block_2.html"
 
 # add FeaturedPostsBlock:
 class FeaturedPostsBlock(StructBlock):
@@ -36,4 +52,5 @@ class PortfolioStreamBlock(BaseStreamBlock):
     # delete the pass statement
 
     card = CardBlock(group="Sections")
+    card2 = CardBlock2(group="Sections")
     featured_posts = FeaturedPostsBlock(group="Sections")
